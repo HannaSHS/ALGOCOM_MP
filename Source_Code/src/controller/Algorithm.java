6819d11645da1368940639fd1d794239e697de4f
Hanna Stefanie Sha <Hanna Stefanie Sha>
2016-08-26 22:25:23 +0800

    Added algorithm class

---
+++ Source_Code/src/controller/Algorithm.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import model.AScores;
+import model.BScores;
+
+/**
+ *
+ * @author Hanna Sha
+ */
+public class Algorithm {
+    
+    ArrayList<AScores> aScoresList;
+    ArrayList<BScores> bScoresList;
+    
+    public void performSectioning(){
+        
+    }
+    
+    public void generateCenters(){
+        
+    }
+    
+}
